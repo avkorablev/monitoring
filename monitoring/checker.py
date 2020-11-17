6275196b842a8eb0c609f977c3c997958bd72a54
Alex Korablev <akorablev@yandex-team.ru>
2020-11-17 18:32:28 +0500

    parametrize tests and check ConnectionError

--- monitoring/checker.py
+++ monitoring/checker.py
@@ -13,13 +13,17 @@ class Rule:
 
 @dataclass
 class CheckResult:
-    response_time: float
-    status_code: int
-    regexp_result: Optional[bool]
+    response_time: Optional[float] = None
+    status_code: Optional[int] = None
+    regexp_result: Optional[bool] = None
+    failed: bool = False  # can be True only if ConnectionError has happened
 
 
 def check(rule: Rule, timeout: float) -> CheckResult:
-    response = requests.request(method=rule.method, url=rule.url, timeout=timeout)
+    try:
+        response = requests.request(method=rule.method, url=rule.url, timeout=timeout)
+    except requests.ConnectionError:
+        return CheckResult(failed=True)
 
     result = CheckResult(
         response_time=response.elapsed.total_seconds(),  # Response time https://stackoverflow.com/a/43260678
--- tests/monitoring/test_checker.py
+++ tests/monitoring/test_checker.py
@@ -1,16 +1,53 @@
+import pytest
 import responses
+from requests import ConnectTimeout
 
 from monitoring.checker import CheckResult, Rule, check
 
 
+test_check_data = [
+    (
+        dict(method=responses.GET, url='http://test.com', status=200),
+        Rule('http://test.com'),
+        CheckResult(10, 200, None)
+    ),
+    (
+        dict(method=responses.GET, url='https://test.com', status=200),
+        Rule('https://test.com'),
+        CheckResult(10, 200, None)
+    ),
+    (
+        dict(method=responses.HEAD, url='http://test.com', status=200),
+        Rule('http://test.com', 'HEAD'),
+        CheckResult(10, 200, None)
+    ),
+    (
+        dict(method=responses.GET, url='http://test.com', status=404),
+        Rule('http://test.com'),
+        CheckResult(10, 404, None)
+    ),
+    (
+        dict(method=responses.GET, url='http://test.com', status=404),
+        Rule('http://test.com'),
+        CheckResult(10, 404, None)
+    ),
+    (
+        dict(method=responses.GET, url='http://test.com', body=ConnectTimeout()),
+        Rule('http://test.com'),
+        CheckResult(failed=True)
+    ),
+]
+
+
 @responses.activate
-def test_check():
-    responses.add(responses.GET, 'http://test.com', status=200)
-    rule = Rule('http://test.com')
-    expected_result = CheckResult(10, 200, None)
+@pytest.mark.parametrize("responses_add, rule, expected_result", test_check_data)
+def test_check(responses_add, rule, expected_result):
+    responses.add(**responses_add)
 
     result = check(rule, 20)
+    assert result.failed == expected_result.failed
     assert result.status_code == expected_result.status_code
     assert result.regexp_result == expected_result.regexp_result
-    assert result.response_time is not None
-    assert result.response_time > 0
+    if not result.failed:
+        assert result.response_time is not None
+        assert result.response_time > 0
