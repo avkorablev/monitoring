183860fd2a0e147a778bfb3abfa6f5882569ea02
Alex Korablev <akorablev@yandex-team.ru>
2020-11-18 18:23:36 +0500

    yaml for storing rules

--- monitoring/worker.py
+++ monitoring/worker.py
@@ -1,17 +1,34 @@
-from typing import Set
+import os
+import re
+from typing import Dict, Set
 
-from monitoring import rules_registry
+import yaml
+
 from monitoring.checker import Rule, check
 
 TIMEOUT = 10
 
 
-def init_rules() -> Set[Rule]:
-    return rules_registry.rules
+def parse_rule(settings: Dict) -> Rule:
+    if 'url' not in settings:
+        raise ValueError('There is an problem with parsing Rule')
+    return Rule(
+        url=settings['url'],
+        method=settings.get('method', None),
+        regexp=None if 'regexp' not in settings else re.compile(settings['regexp'])
+    )
+
+
+def init_rules(yaml_path: str) -> Set[Rule]:
+    if not os.path.exists(yaml_path):
+        raise ValueError('YAML file does not exist')
+    with open(yaml_path, 'r') as f:
+        rules = yaml.safe_load(f)
+    return set((parse_rule(rule) for rule in rules or []))
 
 
 def main():
-    rules = init_rules()
+    rules = init_rules(os.path.join(os.path.dirname(__file__), '..', 'tests', 'rules', 'one_rule.yaml'))
     results = [check(rule, TIMEOUT) for rule in rules]
     print(results)
 
--- tests/monitoring/test_worker.py
+++ tests/monitoring/test_worker.py
@@ -1,5 +1,10 @@
+import os
+import re
+
+import pytest
+
 from monitoring import Rule, rules_registry
-from monitoring.worker import init_rules
+from monitoring.worker import init_rules, parse_rule
 
 
 def test_registry():
@@ -9,6 +14,28 @@ def test_registry():
     assert rules_registry.rules == {rule}
 
 
+def test_init_rules_yaml_not_exist():
+    with pytest.raises(ValueError):
+        init_rules('')
+
+
 def test_init_rules():
-    rules = init_rules()
+    rules = init_rules(os.path.join(os.path.dirname(__file__), '..', 'rules', 'empty.yaml'))
     assert rules == set()
+
+
+def test_init_rules_file_with_rule():
+    rules = init_rules(os.path.join(os.path.dirname(__file__), '..', 'rules', 'one_rule.yaml'))
+    assert rules == {Rule('http://test.com', 'POST', re.compile('.*'))}
+
+
+def test_parse_rule():
+    input_dict = {
+        'url': 'http://test.com',
+        'method': 'POST',
+        'regexp': '.*'
+    }
+
+    expected_rule = Rule('http://test.com', 'POST', re.compile('.*'))
+
+    assert parse_rule(input_dict) == expected_rule
