0c981df70e9d115ba277212c3a482a8cc92e525b
Alex Korablev <akorablev@yandex-team.ru>
2020-11-23 18:02:56 +0500

    clean up code of the consumer

--- monitoring/consumer.py
+++ monitoring/consumer.py
@@ -1,32 +1,42 @@
-import os
+import logging
 from typing import Generator
 
 import psycopg2
 from kafka import KafkaConsumer
 from psycopg2 import sql
 
-from monitoring.checker import CheckResult, Rule
-from monitoring.settings import build_settings, cert_files
+from .checker import CheckResult
+from .settings import build_settings, cert_files
+
+
+logger = logging.getLogger(__name__)
+
+
+SELECT_RULE = sql.SQL('select id from monitoring.rules where url = %s and method = %s and regexp = %s')
+
+
+INSERT_CHECK_RESULT = sql.SQL(
+    'insert into monitoring.checks '
+    '(utc_time, rule_id, response_time, status_code, regexp_result, failed) '
+    'values (%s, %s, %s, %s, %s, %s)'
+)
 
 
 def read_kafka_records(consumer: KafkaConsumer) -> Generator[CheckResult, None, None]:
-        raw_msgs = consumer.poll(timeout_ms=1000)
-        for tp, msgs in raw_msgs.items():
-            for msg in msgs:
-                print(msg.value)
-                yield CheckResult.deserialize(msg.value)
+    raw_msgs = consumer.poll(timeout_ms=1000)
+    for tp, msgs in raw_msgs.items():
+        for msg in msgs:
+            yield CheckResult.deserialize(msg.value)
 
 
-if __name__ == '__main__':
-    settings = build_settings()
-
-    # With code snippets from https://help.aiven.io/en/articles/489572-getting-started-with-aiven-kafka
+# With code snippets from https://help.aiven.io/en/articles/489572-getting-started-with-aiven-kafka
+def get_consumer(settings):
     ssl_cafile, ssl_certfile, ssl_keyfile = cert_files()
-    consumer = KafkaConsumer(
-        'monitoring-tests',
+    return KafkaConsumer(
+        settings['kafka']['topic'],
         auto_offset_reset='earliest',
-        client_id='demo-client-1',
-        group_id='demo-group',
+        client_id=settings['kafka']['consumer']['client_id'],
+        group_id=settings['kafka']['consumer']['group_id'],
         bootstrap_servers=settings['kafka']['url'],
         security_protocol='SSL',
         ssl_cafile=ssl_cafile,
@@ -34,44 +44,35 @@ if __name__ == '__main__':
         ssl_keyfile=ssl_keyfile,
     )
 
+
+def main():
+    settings = build_settings()
+    consumer = get_consumer(settings)
     while True:
         with psycopg2.connect(
-            host=settings['pg']['host'],
-            port=settings['pg']['port'],
-            database=settings['pg']['database'],
-            user=settings['pg']['user'],
-            password=settings['pg']['password'],
-            sslmode=settings['pg']['sslmode'],
+                host=settings['pg']['host'],
+                port=settings['pg']['port'],
+                database=settings['pg']['database'],
+                user=settings['pg']['user'],
+                password=settings['pg']['password'],
+                sslmode=settings['pg']['sslmode'],
         ) as connection, connection.cursor() as cursor:
             for record in read_kafka_records(consumer):
-                print("Received: {}".format(record))
+                logger.debug("Received: %s", record)
                 url, method, regexp = record.rule_id.split(':::')
-                cursor.execute(
-                    sql.SQL(
-                        "select id from {}.{} where url = %s and method = %s and regexp = %s"
-                    ).format(
-                        sql.Identifier('monitoring'),
-                        sql.Identifier('rules')
-                    ),
-                    [url, method, regexp]
-                )
+                cursor.execute(SELECT_RULE, [url, method, regexp])
                 if cursor.rowcount < 1:
-                    print("Record id for {}:::{}:::{} not found".format(url, method, regexp))
+                    logger.warning("Record id for %s not found", record.rule_id)
                     continue
                 rule_id = cursor.fetchone()[0]
-                print()
-
                 cursor.execute(
-                    sql.SQL(
-                        "insert into {}.{} "
-                        "(utc_time, rule_id, response_time, status_code, regexp_result, failed) "
-                        "values (%s, %s, %s, %s, %s, %s)"
-                    ).format(
-                        sql.Identifier('monitoring'),
-                        sql.Identifier('checks')
-                    ),
+                    INSERT_CHECK_RESULT,
                     [record.utc_time, rule_id, record.response_time, record.status_code,
                      record.regexp_result, record.failed]
                 )
 
         consumer.commit()
+
+
+if __name__ == '__main__':
+    main()
